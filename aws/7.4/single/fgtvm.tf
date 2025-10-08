ba4c337c384cbc157d681275da148f771f7cef0e
s0cit0 <s0cit0@users.noreply.github.com>
2025-10-08 18:20:43 -0400

    Simplify deployment to core network and VM

--- aws/7.4/single/fgtvm.tf
+++ aws/7.4/single/fgtvm.tf
@@ -1,31 +1,5 @@
 // FGTVM instance
 
-locals {
-  bootstrap_configuration = templatefile(var.bootstrap_fgtvm, {
-    adminsport = var.adminsport
-  })
-
-  license_file_content = file(var.license)
-
-  license_payload = var.license_format == "token" ? "LICENSE-TOKEN:${chomp(local.license_file_content)} INTERVAL:4 COUNT:4" : local.license_file_content
-
-  s3_bucket_id = var.bucket ? aws_s3_bucket.s3_bucket[0].id : null
-
-  s3_user_data_file = var.bucket ? jsonencode({
-    bucket = local.s3_bucket_id
-    region = var.region
-    license = var.license
-    config  = var.bootstrap_fgtvm
-  }) : null
-
-  s3_user_data_token = var.bucket ? jsonencode({
-    bucket         = local.s3_bucket_id
-    region         = var.region
-    "license-token" = local.license_file_content
-    config         = var.bootstrap_fgtvm
-  }) : null
-}
-
 resource "aws_network_interface" "eth1" {
   description       = "fgtvm-port2"
   subnet_id         = aws_subnet.privatesubnetaz1.id
@@ -38,7 +12,6 @@ resource "aws_network_interface" "eth1" {
 }
 
 resource "aws_network_interface_sg_attachment" "internalattachment" {
-  depends_on           = [aws_network_interface.eth1]
   security_group_id    = aws_security_group.allow_all.id
   network_interface_id = aws_network_interface.eth1.id
 }
@@ -50,25 +23,6 @@ resource "aws_network_interface_attachment" "port2" {
   device_index         = 1
 }
 
-# Cloudinit config in MIME format
-data "cloudinit_config" "config" {
-  gzip          = false
-  base64_encode = false
-
-  # Main cloud-config configuration file.
-  part {
-    filename     = "config"
-    content_type = "text/x-shellscript"
-    content      = local.bootstrap_configuration
-  }
-
-  part {
-    filename     = "license"
-    content_type = "text/plain"
-    content      = local.license_payload
-  }
-}
-
 resource "aws_instance" "fgtvm" {
   //it will use region, architect, and license type to decide which ami to use for deployment
   ami               = var.fgtami[var.region][var.arch][var.license_type]
@@ -77,10 +31,6 @@ resource "aws_instance" "fgtvm" {
   key_name          = var.keyname
   subnet_id         = aws_subnet.publicsubnetaz1.id
   vpc_security_group_ids = [aws_security_group.public_allow.id]
-
-  user_data = var.bucket ? (var.license_format == "file" ? local.s3_user_data_file : local.s3_user_data_token) : data.cloudinit_config.config.rendered
-
-  iam_instance_profile = var.bucket ? aws_iam_instance_profile.fortigate[0].id : null
 
   root_block_device {
     volume_type = "gp2"
